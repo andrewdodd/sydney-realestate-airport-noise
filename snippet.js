185817cf6037edfb0a0d52396b712e37021cbb68
Andrew Dodd <andrew.john.dodd@gmail.com>
2020-11-18 05:12:08 +0100

    Update snippet.js
    Just make the functions functions...that way they can be redefined.

--- snippet.js
+++ snippet.js
@@ -2,7 +2,7 @@
 // console to do map = t.mapRef
 
 // https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects#38327540
-const groupBy = function(xs, key) {
+function groupBy(xs, key) {
   return xs.reduce(function(rv, x) {
     (rv[x[key]] = rv[x[key]] || []).push(x)
     return rv
