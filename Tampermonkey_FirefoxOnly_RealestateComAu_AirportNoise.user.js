7165b3709552052cdebc89e32aec9f862274bdf1
Andrew Dodd <andrew.john.dodd@gmail.com>
2021-03-22 21:56:08 +1100

    Update Tampermonkey_FirefoxOnly_RealestateComAu_AirportNoise.user.js

--- Tampermonkey_FirefoxOnly_RealestateComAu_AirportNoise.user.js
+++ Tampermonkey_FirefoxOnly_RealestateComAu_AirportNoise.user.js
@@ -9,18 +9,23 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
+//console.log("Found scripts before execing", document.getElementsByTagName('script'))
+
 airportNoiseCurveGlobals = {};
 (function() {
     'use strict';
 
+    function patchScript(text) {
+      return text.replace(/this.map=new google.maps.Map\(Q,this.options.googleDefaults\);/g, "this.map=new google.maps.Map(Q,this.options.googleDefaults);airportNoiseCurveGlobals.map=this.map;");
+    }
     function addScript(text) {
         // Hack in to store "this.map" in the airportNoiseCurveGlobals
-        text = text.replace(/this.map=new google.maps.Map\(Q,this.options.googleDefaults\);/g, "this.map=new google.maps.Map(Q,this.options.googleDefaults);airportNoiseCurveGlobals.map=this.map;");
         var newScript = document.createElement('script');
         newScript.type = "text/javascript";
-        newScript.textContent = text;
+        newScript.textContent = patchScript(text);
         var head = document.getElementsByTagName('head')[0];
         head.appendChild(newScript);
+        //document.body.appendChild(newScript);
     }
 
     window.addEventListener('beforescriptexecute', function(e) {
@@ -30,19 +35,21 @@ airportNoiseCurveGlobals = {};
             e.stopPropagation();
             GM_xmlhttpRequest({
                 method: "GET",
-                url: e.target.src,
+                url: src,
                 onload: function(response) {
                     console.error("FETCHED IT");
                     addScript(response.responseText);
                 }
             });
         }
+
     });
 
     function loadNoiseCurvesToMap() {
         let map = airportNoiseCurveGlobals.map;
         if (!map) {
             console.log("NO MAP...Sorry...perhaps try refreshing?")
+            //window.location.reload(true);
             return;
         }
         // https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects#38327540
@@ -89,6 +96,39 @@ airportNoiseCurveGlobals = {};
             return (name + " " + desc).trim()
         }
 
+        function buildToggle(added, map, toggeText, allButtons) {
+            const toggleButton = document.createElement("button");
+            toggleButton.textContent = toggeText;
+            toggleButton.classList.add("custom-map-control-button");
+
+            let shown = false;
+            toggleButton.show = () => {
+                if (!shown) {
+                    toggleButton.classList.add("active");
+                    toggleButton.style.color = "blue";
+                    added.forEach(layer => layer.setMap(map));
+                    shown = true;
+                }
+            };
+            toggleButton.hide = () => {
+                if (shown) {
+                    toggleButton.classList.remove("active");
+                    toggleButton.style.color = null;
+                    added.forEach(layer => layer.setMap(null));
+                    shown = false;
+                }
+            };
+            toggleButton.toggle = () => {
+                if (shown) {
+                    toggleButton.hide();
+                } else {
+                    toggleButton.show();
+                }
+            }
+
+            return toggleButton;
+        }
+
         function buildLayers(polys, map, toggeText) {
             let added = []
             for (var poly of polys) {
@@ -122,28 +162,13 @@ airportNoiseCurveGlobals = {};
                     fillOpacity: 0.3, // styles['fill-opacity'],
                 });
                 p.addListener('click', buildPopupDisplayer(popupText(poly)));
-                p.setMap(map);
+                //p.setMap(map);
 
                 added.push(p);
             }
-            const toggleButton = document.createElement("button");
-            toggleButton.textContent = toggeText;
-            toggleButton.classList.add("custom-map-control-button");
-
-            toggleButton.addEventListener("click", () => {
-                //overlay.toggleDOM(map);
-                for (var layer of added) {
-                    if (layer.getMap() === null) {
-                        layer.setMap(map)
-                    } else {
-                        layer.setMap(null)
-                    }
-                }
-            });
 
             return {
                 layers: added,
-                toggleButton
             }
         }
 
@@ -172,19 +197,35 @@ airportNoiseCurveGlobals = {};
             // Remove the N60 curves...they're not useful
             delete by_group_name['N60 2026']
             delete by_group_name['N60 2033']
+            // Use the '2018 ANEI 2' version, it appears to be better
+            by_group_name['2018 ANEI'] = by_group_name['2018 ANEI 2']
             delete by_group_name['2018 ANEI 2']
+
+            const buttons = [];
 
             for (var group_name in by_group_name) {
                 layers = by_group_name[group_name]
                 layers.sort(compareMetadataValue)
                 const group = buildLayers(by_group_name[group_name], map, group_name)
-                map.controls[google.maps.ControlPosition.TOP_RIGHT].push(group.toggleButton);
-                group.toggleButton.click();
+                const toggleButton = buildToggle(group.layers, map, group_name, buttons);
+                buttons.push(toggleButton);
+
+                toggleButton.addEventListener("click", () => {
+                    toggleButton.toggle();
+                    buttons.filter(b => b !== toggleButton).forEach(b => b.hide());
+                });
+                map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleButton);
             }
         })
 
     };
 
+    function slowTheLoad() {
+        const start = (new Date()).getTime();
+        const end = start + 1000;
+        while ((new Date()).getTime() < end) {}
+    }
     //setTimeout(loadNoiseCurvesToMap, 5000);
     window.addEventListener('load', loadNoiseCurvesToMap);
+    //window.addEventListener('load', slowTheLoad);
 })();
